bb10eb88bb7c8a5fadaa3cba1007c4d1a002ac3d
Andrii Samoiliuk <andrewsamoiliuk@gmail.com>
2022-10-11 17:33:50 +0300

    header started

---
+++ students/samoiliuk_andrii/exam.js/src/js/script.js
@@ -0,0 +1,13 @@
+import $ from "jquery";
+import 'slick-carousel';
+
+$('.bgSlider').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 3000,
+});
+
