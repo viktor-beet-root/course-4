bd131cc11662d2a21e9b9deddab9c6a98a14c865
Tetiana Dmytrenko <tdmitrenko442@gmail.com>
2022-10-25 19:42:48 +0200

    add changes

--- students/dmytrenko_tetiana/menuBurger_scroll/src/js/script.js
+++ students/dmytrenko_tetiana/menuBurger_scroll/src/js/script.js
@@ -85,3 +85,59 @@ if (menuLinks.length > 0) {
         }
     }
 }
+
+// кастомна карта
+
+function initMap() {
+
+    const center = {
+        lat: 40.67560680212675,
+        lng: -73.91454318221241
+    }
+
+    const map = new google.maps.Map(document.getElementById("map"), {
+        center,
+        zoom: 12,
+    });
+    // const image = "https://onlyta.github.io/course-4/students/dmytrenko_tetiana/exem_2/src/images/Pin.svg";
+    const image = "https://imagizer.imageshack.com/img924/5844/rECX12.png";
+    const marker = new google.maps.Marker({
+        position: center,
+        map: map,
+        title: 'Hilltop',
+        icon: image,
+    });
+    const contentString =
+        '<div id="content">' +
+        '<div id="siteNotice">' +
+        "</div>" +
+        '<h1 id="firstHeading" class="firstHeading">Uluru</h1>' +
+        '<div id="bodyContent">' +
+        "<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large " +
+        "sandstone rock formation in the southern part of the " +
+        "Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) " +
+        "south west of the nearest large town, Alice Springs; 450&#160;km " +
+        "(280&#160;mi) by road. Kata Tjuta and Uluru are the two major " +
+        "features of the Uluru - Kata Tjuta National Park. Uluru is " +
+        "sacred to the Pitjantjatjara and Yankunytjatjara, the " +
+        "Aboriginal people of the area. It has many springs, waterholes, " +
+        "rock caves and ancient paintings. Uluru is listed as a World " +
+        "Heritage Site.</p>" +
+        '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">' +
+        "https://en.wikipedia.org/w/index.php?title=Uluru</a> " +
+        "(last visited June 22, 2009).</p>" +
+        "</div>" +
+        "</div>";
+    const infowindow = new google.maps.InfoWindow({
+        content: contentString,
+        ariaLabel: "Uluru",
+    });
+    marker.addListener("click", () => {
+        infowindow.open({
+            anchor: marker,
+            map,
+        });
+    });
+}
+
+window.initMap = initMap;
