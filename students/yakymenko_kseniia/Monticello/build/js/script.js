178020d2d15cfc12f2ce9ea49e7dff6f5e32e686
Kseniia Yakymenko <ksu552005@gmail.com>
2022-10-28 23:09:10 +0300

    monticello_commit

---
+++ students/yakymenko_kseniia/Monticello/build/js/script.js
@@ -0,0 +1,11 @@
+/******/ (function() { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**************************!*\
+  !*** ./src/js/script.js ***!
+  \**************************/
+function scrollToHandler() {
+  console.log(1);
+}
+/******/ })()
+;
+//# sourceMappingURL=script.js.map
