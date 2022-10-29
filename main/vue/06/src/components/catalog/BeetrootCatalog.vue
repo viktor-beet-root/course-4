263400d13f18970a54077a579495898e3e865b81
Viktor Dehtiarov <4059858@gmail.com>
2022-10-29 13:22:52 +0300

    Lesson 45. Vue 6

---
+++ main/vue/06/src/components/catalog/BeetrootCatalog.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <beetroot-title class="title" title="Catalog" />
+        <div class="row">
+            <beetroot-product
+                class="col-4"
+                v-for="product in productList"
+                :key="product.id"
+                :product="product"
+                @addToCart="$emit('addToCart', product)"
+            />
+        </div>
+    </div>
+</template>
+
+<script>
+import BeetrootTitle from "../ui/BeetrootTitle.vue";
+import BeetrootProduct from "./BeetrootProduct.vue";
+
+export default {
+    components: { BeetrootTitle, BeetrootProduct },
+    name: "beetroot-catalog",
+    emits: ["addToCart"],
+    props: {
+        productList: {
+            type: Array,
+            default() {
+                return [];
+            },
+        },
+    },
+};
+</script>
---
+++ main/vue/06/src/components/catalog/BeetrootProduct.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="product mb-4">
+        <beetroot-image
+            class="product__image"
+            :src="product.img"
+            :alt="product.name"
+        />
+        <beetroot-title
+            class="product__title fw-bold"
+            :level="2"
+            :title="product.name"
+        />
+        <beetroot-price
+            :price="product.price"
+            :specialPrice="product.specialPrice"
+        />
+        <beetroot-button
+            class="product__add-to-cart btn btn-primary w-100"
+            text="Add To Cart"
+            :disabled="!product.instock"
+            @action="$emit('addToCart')"
+        />
+    </div>
+</template>
+
+<script>
+import BeetrootTitle from "../ui/BeetrootTitle.vue";
+import BeetrootImage from "../ui/BeetrootImage.vue";
+import BeetrootPrice from "../BeetrootPrice.vue";
+import BeetrootButton from "../ui/BeetrootButton.vue";
+
+export default {
+    components: { BeetrootImage, BeetrootTitle, BeetrootPrice, BeetrootButton },
+    name: "beetroot-product",
+    emits: ["addToCart"],
+    props: {
+        product: {
+            type: Object,
+            default() {
+                return {};
+            },
+        },
+    },
+};
+</script>
